51617493e0ef742127fb9f91ccb03bd7012e165a
David_Collins <dcollins4096@gmail>
2026-03-04 12:28:33 -0500

    tweaks

--- p79d_kyes/run.py
+++ p79d_kyes/run.py
@@ -21,6 +21,7 @@ if not os.path.exists('dtools/starter1.py'):
     print("Need to get the submodule")
     print("git submodule init")
     print("git submodule update")
+    print("cd dtools; git checkout main")
     sys.exit(-1)
 
 
